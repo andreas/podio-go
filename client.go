5206f4c0cf9ba8f9346d62223e90648c2ca3d706
Andreas Garnæs <andreas@garnaes.dk>
2014-09-19 19:07:26 +0100

    Add app authentication.
    Ensure response bodies are closed.

--- client.go
+++ client.go
@@ -92,13 +92,44 @@ func AuthWithUserCredentials(client_id string, client_secret string, username st
 		"client_secret": {client_secret},
 	}
 	resp, err := http.PostForm("https://api.podio.com/oauth/token", data)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
+	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
+	err = json.Unmarshal(respBody, &authToken)
+	if err != nil {
+		return nil, err
+	}
+
+	return &authToken, nil
+}
+
+func AuthWithAppCredentials(client_id, client_secret string, app_id uint, app_token string) (*AuthToken, error) {
+	var authToken AuthToken
+
+	data := url.Values{
+		"grant_type":    {"app"},
+		"app_id":        {fmt.Sprintf("%d", app_id)},
+		"app_token":     {app_token},
+		"client_id":     {client_id},
+		"client_secret": {client_secret},
+	}
+	resp, err := http.PostForm("https://api.podio.com/oauth/token", data)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
 	respBody, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
 
 	err = json.Unmarshal(respBody, &authToken)
 	if err != nil {
