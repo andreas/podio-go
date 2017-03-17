4f204d9642659279520094339088d94f3d29ed1c
Brian Stengaard <brian@stengaard.eu>
2017-03-17 11:23:16 -0400

    Use correct path for GetSpaceByOrgAndSlug.
    Fixes #21

--- space.go
+++ space.go
@@ -25,7 +25,7 @@ func (client *Client) GetSpace(id int64) (space *Space, err error) {
 }
 
 func (client *Client) GetSpaceByOrgIdAndSlug(orgId int64, slug string) (space *Space, err error) {
-	path := fmt.Sprintf("/space/org/%d/%s", orgId, slug)
+	path := fmt.Sprintf("/space/org/%d/url/%s", orgId, slug)
 	err = client.Request("GET", path, nil, nil, &space)
 	return
 }
