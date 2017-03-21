4ab1542d00af2878285250af0e8ef900f4a0725e
Brian Stengaard <brian@stengaard.eu>
2017-03-21 19:58:14 -0400

    Add field settings and tests for unmarshalling (#25)
    * Always use UTC time for date field values
    
    Old solution would incorrectly parse the supplied time as UTC even
    though it would be localised according to Podio settings.
    
    * Show field configuration on field values
    
    * wip: add initial support and infrastructure for field settings
    
    * Add more field setting types
    
    * Tel field values are strings not ints
    
    * Add email and phone field
    
    * Add more field settings types
    
    * ImageFieldSettings
    * ContactFieldSettings
    * CalculationFieldSettings
    * LocationFieldSettings
    * DurationFieldSettings
    
    Add tests for the above

--- item.go
+++ item.go
@@ -3,7 +3,6 @@ package podio
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
 )
 
 // Item describes a Podio item object
@@ -22,14 +21,24 @@ type Item struct {
 	Link               string   `json:"link"`
 	Revision           int      `json:"revision"`
 	Push               Push     `json:"push"`
+	ExternalId         string   `json:"external_id"`
 }
 
 // partialField is used for JSON unmarshalling
 type partialField struct {
-	Id         int64           `json:"field_id"`
-	ExternalId string          `json:"external_id"`
-	Type       string          `json:"type"`
-	Label      string          `json:"label"`
+	Id         int64  `json:"field_id"`
+	ExternalId string `json:"external_id"`
+	Type       string `json:"type"`
+	Label      string `json:"label"`
+	Config     struct {
+		Description   string          `json:"description"`
+		Required      bool            `json:"required"`
+		HiddenIfEmpty bool            `json:"hidden"`
+		AlwaysHidden  bool            `json:"hidden_create_view_edit"`
+		ConfigVersion int             `json:"delta"`
+		SettingsJSON  json.RawMessage `json:"settings"`
+		Settings      interface{}     `json:"-"`
+	} `json:"config"`
 	ValuesJSON json.RawMessage `json:"values"`
 }
 
@@ -39,93 +48,121 @@ type Field struct {
 	Values interface{}
 }
 
-func (f *Field) unmarshalValuesInto(out interface{}) error {
-	if err := json.Unmarshal(f.ValuesJSON, &out); err != nil {
-		return fmt.Errorf("[ERR] Cannot unmarshal %s into %s: %v\n", f.ValuesJSON, reflect.TypeOf(out), err)
+func (f *Field) unmarshalInto(val, settings interface{}) error {
+	if err := json.Unmarshal(f.ValuesJSON, val); err != nil {
+		return fmt.Errorf("cannot unmarshal %q into %T: %v", f.ValuesJSON, val, err)
 	}
+
+	// allow for tests that does not set field configs.
+	if settings != nil && len(f.Config.SettingsJSON) > 0 {
+		if err := json.Unmarshal(f.Config.SettingsJSON, settings); err != nil {
+			return fmt.Errorf("cannot unmarshal %q into %T: %v", f.Config.SettingsJSON, settings, err)
+		}
+	}
+
 	return nil
 }
+
+// Used during testing.
+var errOnUnknownField bool
 
 func (f *Field) UnmarshalJSON(data []byte) error {
 	f.partialField = partialField{}
 	if err := json.Unmarshal(data, &f.partialField); err != nil {
 		return err
 	}
+	var err error
 
 	switch f.Type {
 	case "app":
-		values := []AppValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []AppValue{}, AppFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "date":
-		values := []DateValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []DateValue{}, DateFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "text":
-		values := []TextValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []TextValue{}, TextFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "number":
-		values := []NumberValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []NumberValue{}, NumberFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "image":
-		values := []ImageValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []ImageValue{}, ImageFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "member":
 		values := []MemberValue{}
-		f.unmarshalValuesInto(&values)
+		err = f.unmarshalInto(&values, nil)
 		f.Values = values
 	case "contact":
-		values := []ContactValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []ContactValue{}, ContactFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "money":
-		values := []MoneyValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []MoneyValue{}, MoneyFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "progress":
+		// progress fields has no field specific settings
 		values := []ProgressValue{}
-		f.unmarshalValuesInto(&values)
+		err = f.unmarshalInto(&values, nil)
 		f.Values = values
 	case "location":
-		values := []LocationValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []LocationValue{}, LocationFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "video":
 		values := []VideoValue{}
-		f.unmarshalValuesInto(&values)
+		err = f.unmarshalInto(&values, nil)
 		f.Values = values
 	case "duration":
-		values := []DurationValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []DurationValue{}, DurationFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "embed":
+		// embed fields has no field specific settings
 		values := []EmbedValue{}
-		f.unmarshalValuesInto(&values)
+		err = f.unmarshalInto(&values, nil)
 		f.Values = values
 	case "question":
 		values := []QuestionValue{}
-		f.unmarshalValuesInto(&values)
+		err = f.unmarshalInto(&values, nil)
 		f.Values = values
 	case "category":
-		values := []CategoryValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []CategoryValue{}, CategoryFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	case "tel":
 		values := []TelValue{}
-		f.unmarshalValuesInto(&values)
+		err = f.unmarshalInto(&values, nil)
 		f.Values = values
 	case "calculation":
-		values := []CalculationValue{}
-		f.unmarshalValuesInto(&values)
-		f.Values = values
+		values, cfg := []CalculationValue{}, CalculationFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
+	case "phone":
+		values, cfg := []PhoneValue{}, PhoneFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
+	case "email":
+		values, cfg := []EmailValue{}, EmailFieldSettings{}
+		err = f.unmarshalInto(&values, &cfg)
+		f.Values, f.Config.Settings = values, cfg
 	default:
+		if errOnUnknownField {
+			return fmt.Errorf("unknown field type %q", f.Type)
+		}
 		// Unknown field type
-		values := []interface{}{}
-		f.unmarshalValuesInto(&values)
+		values, cfg := []interface{}{}, map[string]interface{}{}
+		err = f.unmarshalInto(&values, &cfg)
 		f.Values = values
+	}
+	if err != nil {
+		return err
 	}
 
 	f.ValuesJSON = nil
@@ -137,9 +174,21 @@ type TextValue struct {
 	Value string `json:"value"`
 }
 
+// TextFieldSettings is the configuration of a text field
+type TextFieldSettings struct {
+	Format string `json:"format"`
+	Size   string `json:"size"`
+}
+
 // NumberValue is the value for fields of type `number`
 type NumberValue struct {
 	Value float64 `json:"value,string"`
+}
+
+// NumberFieldSettings describes the settings for a number field
+type NumberFieldSettings struct {
+	// Indicates how many decimals to show
+	Decimals int `json:"decimals"`
 }
 
 // Image is the value for fields of type `image`
@@ -147,15 +196,48 @@ type ImageValue struct {
 	Value File `json:"value"`
 }
 
+// ImageFieldSettings describes which mime types are allowed on an image field
+type ImageFieldSettings struct {
+	AllowedMimeypes []string `json:"allowed_mimetypes"`
+}
+
 // DateValue is the value for fields of type `date`
 type DateValue struct {
-	Start *Time `json:"start"`
-	End   *Time `json:"end"`
+	Start *Time `json:"start_utc"`
+	End   *Time `json:"end_utc"`
+}
+
+// DateFieldSettings defines the capabilities of a date field
+type DateFieldSettings struct {
+	// Is this field shown on the calendar?
+	Calendar bool `json:"calendar"`
+
+	// Indicates if the end time is valid
+	//   "enabled" means it is
+	//   "disabled" means that the time end time can be ignored
+	End string `json:"end"`
+
+	// Indicates if the field is a time field or date-only field.
+	//   "enabled" means that time is enabled on the field
+	//   "disabled" means that the field is date-only and the time fragment can be ignored
+	Time string `json:"time"`
 }
 
 // AppValue is the value for fields of type `app`
 type AppValue struct {
 	Value Item `json:"value"`
+}
+
+// AppFieldSettings defines which apps an app field can contain.
+type AppFieldSettings struct {
+	Mulitple       bool `json:"multiple"`
+	ReferencedApps []struct {
+		AppId int `json:"app_id"`
+		App   App `json:"app"`
+
+		//0 means all items in the app
+		ViewId int `json:"view_id"`
+	} `json:"referenced_apps"`
 }
 
 // MemberValue is the value for fields of type `member`
@@ -168,10 +250,21 @@ type ContactValue struct {
 	Value Contact `json:"value"`
 }
 
+// ContactFieldSettings describes the kind of contacts allowed in a contact field
+type ContactFieldSettings struct {
+	Type       string   `json:"type"`
+	ValidTypes []string `json:"valid_types"`
+}
+
 // MoneyValue is the value for fields of type `money`
 type MoneyValue struct {
 	Value    float64 `json:"value,string"`
 	Currency string  `json:"currency"`
+}
+
+// MoneyFieldSettings describes the allowed currencies in a money field
+type MoneyFieldSettings struct {
+	AllowedCurrencies []string `json:"allowed_currencies"`
 }
 
 // ProgressValue is the value for fields of type `progress`
@@ -181,16 +274,22 @@ type ProgressValue struct {
 
 // LocationValue is the value for fields of type `location`
 type LocationValue struct {
-	Value        string `json:"value"`
-	Formatted    string `json:"formatted"`
-	StreetNumber string `json:"street_number"`
-	StreetName   string `json:"street_name"`
-	PostalCode   string `json:"postal_code"`
-	City         string `json:"city"`
-	State        string `json:"state"`
-	Country      string `json:"country"`
-	Lat          string `json:"lat"`
-	Lng          string `json:"lng"`
+	Value        string  `json:"value"`
+	Formatted    string  `json:"formatted"`
+	StreetNumber string  `json:"street_number"`
+	StreetName   string  `json:"street_name"`
+	PostalCode   string  `json:"postal_code"`
+	City         string  `json:"city"`
+	State        string  `json:"state"`
+	Country      string  `json:"country"`
+	Lat          float64 `json:"lat"`
+	Lng          float64 `json:"lng"`
+}
+
+// LocationFieldSettings defines whether or not to a map should be shown for a location field
+type LocationFieldSettings struct {
+	Structured bool `json:"structured"`
+	HasMap     bool `json:"has_map"`
 }
 
 // VideoValue is the value for fields of type `video`
@@ -203,20 +302,37 @@ type DurationValue struct {
 	Value int `json:"value"`
 }
 
+// DurationFieldSettings defines which parts of the duration to render
+type DurationFieldSettings struct {
+	// Can be days, hours, minutes and/or seconds
+	Fields []string `json:"fields"`
+}
+
 // EmbedValue is the value for fields of type `embed`
 type EmbedValue struct {
 	Embed Embed `json:"embed"`
 	File  File  `json:"file"`
 }
 
+// CategoryOption is a possible value for a category field
+type CategoryOption struct {
+	Status string `json:"status"`
+	Text   string `json:"text"`
+	Id     int    `json:"id"`
+	Color  string `json:"color"`
+}
+
 // CategoryValue is the value for fields of type `category`
 type CategoryValue struct {
-	Value struct {
-		Status string `json:"status"`
-		Text   string `json:"text"`
-		Id     int    `json:"id"`
-		Color  string `json:"color"`
-	} `json:"value"`
+	Value CategoryOption `json:"value"`
+}
+
+// CategoryFieldSettings holds the configuration of category fields, along with
+// the possible values for the category field.
+type CategoryFieldSettings struct {
+	Multiple bool   `json:"multiple"`
+	Display  string `json:"display"`
+	Options  []CategoryOption
 }
 
 // QuestionValue is the value for fields of type `question`
@@ -224,14 +340,55 @@ type QuestionValue struct {
 	Value int `json:"value"`
 }
 
-// TelValue is the value for fields of type `tel`
+// TelValue is the value for fields of type `tel`. This is a deprecated field type
 type TelValue struct {
-	Value int    `json:"value"`
+	Value string `json:"value"`
 	URI   string `json:"uri"`
 }
 
-// CalcationValue is the value for fields of type `calculation` (currently untyped)
-type CalculationValue map[string]interface{}
+// PhoneValue contains the value of a phone field - that is phone numbers.
+type PhoneValue struct {
+	Value string
+	Type  string // Home, work, fax ...
+}
+
+// PhoneFieldSettings defines the settings for the given phone field.
+type PhoneFieldSettings struct {
+	CallLinkScheme string   `json:"call_link_scheme"` // callto://
+	PossibleTypes  []string `json:"possible_types"`
+}
+
+// EmailValue holds email information of contacts fields.
+type EmailValue struct {
+	Value string // The actual email
+	Type  string // home or work email?
+}
+
+// EmailFieldSettings carries the configuration of an email field
+type EmailFieldSettings struct {
+	IncludeInBCC  bool     `json:"include_in_bcc"`
+	IncludeInCC   bool     `json:"include_in_cc"`
+	PossibleTypes []string `json:"possible_types"`
+}
+
+// CalculationValue is the value for fields of type calculation
+type CalculationValue struct {
+	Value string `json:"value"`
+}
+
+// CalculationFieldSettings holds information about a calculation field, including the
+// script it runs and what return type it has.
+type CalculationFieldSettings struct {
+	Script     string `json:"script"`
+	ReturnType string `json:"return_type"`
+
+	Color      string `json:"color"`
+	Expression string `json:"expression"`
+	Time       string `json:"time"`
+	Calendar   string `json:"calendar"`
+	Decimals   int    `json:"decimals"`
+	Unit       string `json:"unit"`
+}
 
 type ItemList struct {
 	Filtered int     `json:"filtered"`
--- item_test.go
+++ item_test.go
@@ -2,8 +2,13 @@ package podio
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"testing"
 
+	"reflect"
+
+	"github.com/kr/pretty"
 	"github.com/stretchr/testify/require"
 )
 
@@ -47,4 +52,337 @@ func TestUnmarshalAppValue(t *testing.T) {
 	r.True(ok, "Expected values to be []podio.AppValue, is %#v", field.Values)
 	r.Len(values, 1)
 	r.Equal(values[0].Value.Id, int64(1))
+}
+
+// To add more tests using the fixtures simply generate the situation you are trying to
+// reproduce in Podio, fetch the results from https://developers.podio.com/ and add the JSON
+// data to the fixtures directory. This sidesteps the authentication dance in testing.
+
+// TestUnmarshalFieldValuesAndSettings tests that all values in a given Podio item can be
+// unmarshalled correctly. The fixture json files are simply the output from
+// https://developers.podio.com/doc/items/get-item-22360 with appropriate item IDs
+func TestUnmarshalFieldValuesAndSettings(t *testing.T) {
+
+	errOnUnknownField = true
+	defer func() {
+		errOnUnknownField = false
+	}()
+
+	type fv struct {
+		ignore   string
+		values   interface{}
+		settings interface{}
+	}
+
+	testCases := []struct {
+		jsonFile string
+		fields   []fv
+	}{
+		{
+			jsonFile: "fixtures/item_225607452.json",
+			fields: []fv{
+				{
+					values:   []TextValue{{"Title"}},
+					settings: TextFieldSettings{"plain", "small"},
+				},
+				{
+					values: []CategoryValue{
+						{CategoryOption{Status: "active", Text: "B", Id: 2, Color: "DCEBD8"}},
+					},
+					settings: CategoryFieldSettings{Display: "inline", Options: []CategoryOption{
+						{Status: "active", Text: "A", Id: 1, Color: "DCEBD8"},
+						{Status: "active", Text: "B", Id: 2, Color: "DCEBD8"},
+						{Status: "active", Text: "C", Id: 3, Color: "DCEBD8"},
+					}},
+				},
+				{
+					values: []DateValue{{
+						Start: parseTime(t, "2014-12-11 22:00:00"),
+					}},
+					settings: DateFieldSettings{
+						Calendar: true, End: "enabled", Time: "enabled",
+					},
+				},
+				{
+					values: []ContactValue{{Value: Contact{
+						UserId: 2468975,
+						Type:   "user",
+						Image: File{
+							Id:   125807791,
+							Link: "https://d2cmuesa4snpwn.cloudfront.net/public/125807791",
+						},
+						ProfileId:  140798621,
+						Link:       "https://podio.com/users/2468975",
+						Avatar:     125807791,
+						Name:       "Brian Stengaard",
+						LastSeenOn: parseTime(t, "2014-12-10 15:26:35"),
+					}}},
+					settings: ContactFieldSettings{"space_users", []string{"user"}},
+				},
+				{
+					values:   []NumberValue{{6513.5100}},
+					settings: NumberFieldSettings{0},
+				},
+				{
+					values:   []MoneyValue{{541.987, "EUR"}},
+					settings: MoneyFieldSettings{[]string{"USD", "EUR"}},
+				},
+				{
+					values: []EmbedValue{{Embed: Embed{
+						Id:          55017316,
+						Type:        "link",
+						Title:       "Google",
+						Description: "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking for.",
+						URL:         "http://google.com/",
+						OriginalURL: "http://google.com/",
+						ResolvedURL: "http://google.com/",
+						Hostname:    "google.com",
+					}}},
+				},
+			},
+		},
+		{
+			jsonFile: "fixtures/item_350017179.json",
+			fields: []fv{
+				{
+					values:   []TextValue{{"incom"}},
+					settings: TextFieldSettings{"plain", "small"},
+				},
+				{
+					values:   []TextValue{{"<p>sadfas</p>"}},
+					settings: TextFieldSettings{"html", "large"},
+				},
+				{
+					values:   []PhoneValue{{"+19197936102", "work"}},
+					settings: PhoneFieldSettings{"callto", []string{"mobile", "work", "home", "main", "work_fax", "private_fax", "other"}},
+				},
+				{
+					values:   []EmailValue{{"hhah@podio.com", "work"}},
+					settings: EmailFieldSettings{PossibleTypes: []string{"work", "home", "other"}},
+				},
+				{
+					values: []CategoryValue{{Value: CategoryOption{Status: "active", Text: "C", Id: 3, Color: "DCEBD8"}}},
+					settings: CategoryFieldSettings{
+						Display: "inline",
+						Options: []CategoryOption{
+							{"active", "A", 1, "DCEBD8"},
+							{"active", "B", 2, "DCEBD8"},
+							{"active", "C", 3, "DCEBD8"},
+						},
+					},
+				},
+				{
+					values:   []TextValue{{"1012312312313123123123123123123123123123123"}},
+					settings: TextFieldSettings{"plain", "small"},
+				},
+				{
+					values: []CalculationValue{{"111.0000"}},
+					settings: CalculationFieldSettings{
+						Script:     "var a = @[Number](field_80608151);\nparseInt(a, 10);",
+						ReturnType: "number",
+						Decimals:   2,
+					},
+				},
+				{
+					values: []DateValue{{
+						Start: parseTime(t, "2016-06-24 19:00:00"),
+						End:   parseTime(t, "2016-06-30 20:00:00"),
+					}},
+					settings: DateFieldSettings{Calendar: true, End: "enabled", Time: "enabled"},
+				},
+				{ignore: "same as the test above. Repeat field."},
+				{
+					values: []AppValue{{Value: Item{
+						Id: 331300398, AppItemId: 3, Title: "sadf 41`", Revision: 14,
+						Link:  "https://podio.com/podio/sandbox-4fcx2i/apps/allfields/items/3",
+						Files: []*File{},
+						Space: Space{
+							Id:       2720177,
+							Name:     "sandbox",
+							URL:      "https://podio.com/podio/sandbox-4fcx2i",
+							URLLabel: "sandbox-4fcx2i",
+							OrgId:    736,
+						},
+						App: App{
+							Id:              10421272,
+							Name:            "AllFields",
+							Status:          "active",
+							Link:            "https://podio.com/podio/sandbox-4fcx2i/apps/allfields",
+							URL:             "https://podio.com/podio/sandbox-4fcx2i/apps/allfields",
+							URLAdd:          "https://podio.com/podio/sandbox-4fcx2i/apps/allfields/items/new",
+							LinkAdd:         "https://podio.com/podio/sandbox-4fcx2i/apps/allfields/items/new",
+							CurrentRevision: 25,
+							ItemName:        "Merge",
+							URLLabel:        "allfields",
+							SpaceId:         2720177,
+							IconId:          251,
+							Icon:            "251.png",
+						},
+						CreatedVia: Via{Id: 1, Name: "Podio"},
+						CreatedOn:  *parseTime(t, "2015-10-09 12:49:43"),
+					}}},
+					settings: AppFieldSettings{
+						Mulitple: true,
+						ReferencedApps: []struct {
+							AppId  int `json:"app_id"`
+							App    App `json:"app"`
+							ViewId int `json:"view_id"`
+						}{{
+							AppId: 10421272,
+							App: App{
+								Id:              10421272,
+								Name:            "AllFields",
+								Status:          "active",
+								Link:            "https://podio.com/podio/sandbox-4fcx2i/apps/allfields",
+								URL:             "https://podio.com/podio/sandbox-4fcx2i/apps/allfields",
+								URLAdd:          "https://podio.com/podio/sandbox-4fcx2i/apps/allfields/items/new",
+								LinkAdd:         "https://podio.com/podio/sandbox-4fcx2i/apps/allfields/items/new",
+								CurrentRevision: 25,
+								ItemName:        "Merge",
+								URLLabel:        "allfields",
+								SpaceId:         2720177,
+								IconId:          251,
+								Icon:            "251.png",
+							},
+							ViewId: 0,
+						}, {
+							AppId: 10118971,
+							App: App{
+								Id:     10118971,
+								Name:   "GPG Keys",
+								Status: "active", URLAdd: "https://podio.com/podio/sandbox-4fcx2i/apps/gpg-keys/items/new",
+								IconId:          14,
+								LinkAdd:         "https://podio.com/podio/sandbox-4fcx2i/apps/gpg-keys/items/new",
+								CurrentRevision: 6,
+								ItemName:        "GPG Key",
+								Link:            "https://podio.com/podio/sandbox-4fcx2i/apps/gpg-keys",
+								URL:             "https://podio.com/podio/sandbox-4fcx2i/apps/gpg-keys",
+								URLLabel:        "gpg-keys",
+								SpaceId:         2720177,
+								Icon:            "14.png",
+							},
+							ViewId: 0,
+						}},
+					},
+				},
+				{ignore: "Slight variaion of calculation field. Still with number output"},
+				{
+					values: []ContactValue{{Value: Contact{
+						UserId:  2468975,
+						SpaceId: 0,
+						Type:    "user",
+						Image: File{
+							Id:   297530466,
+							Link: "https://d2cmuesa4snpwn.cloudfront.net/public/297530466",
+						},
+						ProfileId:  140798621,
+						Link:       "https://podio.com/users/2468975",
+						Avatar:     297530466,
+						Name:       "Brian Stengaard",
+						LastSeenOn: parseTime(t, "2017-03-21 15:43:34"),
+					}}},
+					settings: ContactFieldSettings{"space_users", []string{"user"}},
+				},
+				{ignore: "we already have a test for Number field"},
+				{
+					values: []ImageValue{{Value: File{
+						Id:   195174666,
+						Name: "a1.jpeg",
+						Link: "https://files.podio.com/195174666",
+						Size: 45326,
+					}}},
+					settings: ImageFieldSettings{[]string{"image/png"}},
+				},
+				{ignore: "type money already covered"},
+				{
+					values: []EmbedValue{{
+						Embed: Embed{Id: 117018345, Type: "link", Title: "Google", Description: "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking for.", EmbedHTML: "", URL: "https://google.com/", OriginalURL: "https://google.com/", ResolvedURL: "https://google.com/", Hostname: "google.com", EmbedHeight: 0, EmbedWidth: 0},
+						File:  File{Id: 231335541, Link: "https://files.podio.com/231335541"},
+					}},
+				},
+			},
+		},
+		{
+			jsonFile: "fixtures/item_582709679.json",
+			fields: []fv{
+				{values: []ProgressValue{{Value: 42}}},
+				{
+					values: []LocationValue{{
+						Value:      "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France",
+						Formatted:  "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France",
+						PostalCode: "75007",
+						City:       "Paris",
+						State:      "Île-de-France",
+						Country:    "France",
+						Lat:        48.8583701,
+						Lng:        2.2944813}},
+					settings: LocationFieldSettings{false, true},
+				},
+				{values: []DurationValue{{93784}}, settings: DurationFieldSettings{[]string{"days", "hours", "minutes", "seconds"}}},
+			},
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run("field values test of "+c.jsonFile, func(t *testing.T) {
+			t.Logf("Checking field values on %q", c.jsonFile)
+
+			item := &Item{}
+			err := json.Unmarshal(getFixtureJSON(t, c.jsonFile), item)
+			if err != nil {
+				t.Errorf("Could not unmarshal item: %s", err)
+				return
+			}
+
+			if len(c.fields) != len(item.Fields) {
+				t.Errorf("expected %d fields, but found %d", len(c.fields), len(item.Fields))
+			}
+			for i, f := range item.Fields {
+				if i >= len(c.fields) {
+					break
+				}
+				if c.fields[i].ignore != "" {
+					continue
+				}
+
+				if !reflect.DeepEqual(f.Values, c.fields[i].values) {
+					t.Errorf("mismatching values on field num %d: %s (type: %s field_id: %d)\nGot: %# v\nExp: %# v\nDiff: %v\n",
+						i+1, f.Label, f.Type, f.Id,
+						pretty.Formatter(f.Values),
+						pretty.Formatter(c.fields[i].values),
+						pretty.Diff(f.Values, c.fields[i].values))
+				}
+
+				if !reflect.DeepEqual(f.Config.Settings, c.fields[i].settings) {
+					t.Errorf("mismatching settings on field num %d: %s (type: %s field_id: %d)\nGot: %#v\nExp: %#v\nDiff: %v\n",
+						i+1, f.Label, f.Type, f.Id,
+						pretty.Formatter(f.Config.Settings),
+						pretty.Formatter(c.fields[i].settings),
+						pretty.Diff(f.Config.Settings, c.fields[i].settings))
+				}
+			}
+		})
+	}
+}
+
+// parseTime is a test helper to parse the UTC timestamps found various places in Podio data
+func parseTime(t *testing.T, tim string) *Time {
+	out := &Time{}
+
+	err := out.UnmarshalJSON([]byte(fmt.Sprintf("%q", tim)))
+	if err != nil {
+		t.Fatalf("could not parse time %q as a Podio time: %v", tim, err)
+	}
+	return out
+}
+
+// getFixtureJSON reads a fixture file and returns the raw buffer. It aborts the
+// test framework if an error occurs while reading the file.
+func getFixtureJSON(t *testing.T, name string) []byte {
+	buf, err := ioutil.ReadFile(name)
+	if err != nil {
+		t.Fatalf("Could not fetch fixture file: %v\n", err)
+	}
+
+	return buf
 }
